a17f0fb2e722575077f1fadd5a6a723c3556c2e4
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2021-10-21 20:50:33 -0300

    implementation of connection via socket.io, server initialization via http and addition of colors to release external access

--- back-node/src/app.ts
+++ back-node/src/app.ts
@@ -1,8 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 app.use(router);
 
@@ -15,6 +32,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => {
-  console.log('Started server!');
-});
+export { serverHttp, io };
--- back-node/src/routes.ts
+++ back-node/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './Controllers/AuthenticateUserController';
 import { CreateMessageController } from './Controllers/CreateMessageController';
+import { GetLastThreeMessagesController } from './Controllers/GetLastThreeMessagesController';
+import { ProfileUserController } from './Controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
@@ -8,5 +10,10 @@ const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle)
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+
+router.get('/messages/last3', new GetLastThreeMessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
+
 
 export { router };
