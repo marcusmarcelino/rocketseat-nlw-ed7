b8041778390a4e03ade1e42a15c398d04b495716
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2021-10-21 19:30:40 -0300

    services and routes for creating messages implemented

--- back-node/src/routes.ts
+++ back-node/src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './Controllers/AuthenticateUserController';
+import { CreateMessageController } from './Controllers/CreateMessageController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
-//new AuthenticateUserController().handle
+
 router.post('/authenticate', new AuthenticateUserController().handle)
+
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 
 export { router };
